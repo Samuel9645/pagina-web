4d30f537699c870818678e6ad09679e765cbeb57
Samuel9645 <alu0101545714@ull.edu.es>
2026-02-05 08:17:53 +0000

    Prueba gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://Samuel9645.github.io',
+  base: '/pagina-web/',
+});
