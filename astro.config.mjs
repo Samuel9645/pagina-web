d25f0f4f320b4508714e12fb88245bd3326bf1c0
Samuel9645 <alu0101545714@ull.edu.es>
2026-02-06 13:36:18 +0000

    Probando cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://Samuel9645.github.io',
-  // base: '/pagina-web/',
 
   vite: {
     plugins: [tailwindcss()],
