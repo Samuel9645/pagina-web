ab256aa180182d7890716064430f1f506054eebd
Samuel9645 <alu0101545714@ull.edu.es>
2026-02-06 13:22:28 +0000

    Cambiado config para CloudFlare

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Samuel9645.github.io',
-  base: '/pagina-web/',
+  // site: 'https://Samuel9645.github.io',
+  // base: '/pagina-web/',
 
   vite: {
     plugins: [tailwindcss()],
